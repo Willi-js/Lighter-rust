e186cc0e5af7c423aa9f5c44d42ce50268fb3808
Willi-js <willi.mails.js@gmail.com>
2024-09-26 01:52:34 +0300

    that extra space was annoying me

--- src/fonts.rs
+++ src/fonts.rs
@@ -4,7 +4,7 @@ use iced::Font;
 
 pub static DEFAULT_FONT: Font = Font::with_name("Iosevka Term");
 
-pub fn load_font() ->  Vec<Cow<'static, [u8]>> {
+pub fn load_font() -> Vec<Cow<'static, [u8]>> {
     vec![
         include_bytes!("../fonts/iosevka-term-regular.ttf")
             .as_slice()
